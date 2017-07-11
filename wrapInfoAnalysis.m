0fcdfc798a4916db04fa8e83fe70dd1e70071cbe
David Tingley <davidtingley2@gmail.com>
2017-07-11 16:28:58 -0400

    shuffle control worked in

--- wrapInfoAnalysis.m
+++ wrapInfoAnalysis.m
@@ -30,27 +30,50 @@ for smoothing = 1:round(nBins/2)
            for trial = 1:size(binnedPhaseMap{cond},2)
               binnedPhaseMap_smooth{cond}(cell,trial,:) = circ_smoothTS(squeeze(binnedPhaseMap{cond}(cell,trial,:)),smoothing,'method','mean'); 
               binnedPhaseMap_smooth{cond}(cell,trial,binnedPhaseMap_smooth{cond}(cell,trial,:)==0)=nan;
-              
               rateMap_smooth{cond}(cell,trial,:) = smooth(squeeze(countMap{cond}(cell,trial,:))',smoothing);
+              for iter = 1:10
+                  binnedPhaseMap_smooth_shuffle{cond}(cell,trial,iter,:) = ...
+                      circ_smoothTS(circshift(squeeze(binnedPhaseMap{cond}(cell,trial,:)),round(nBins*rand)),smoothing,'method','mean'); 
+                  
+                  binnedPhaseMap_smooth_shuffle{cond}(cell,trial,iter,...
+                      binnedPhaseMap_smooth_shuffle{cond}(cell,trial,iter,:)==0)=nan;
+                  
+                  rateMap_smooth_shuffle{cond}(cell,trial,iter,:) = ...
+                      smooth(circshift(squeeze(countMap{cond}(cell,trial,:)),round(nBins*rand)),smoothing);
+              end
            end
            range = 0:max(max(squeeze(rateMap_smooth{cond}(cell,:,:))))./63:max(max(squeeze(rateMap_smooth{cond}(cell,:,:))));
            if isempty(range)
                range = [0 1];
            end
+           % discretize actual data
            f = find(rateMap_smooth{cond}(cell,:)==0);
            rateMap_smooth{cond}(cell,f)=nan;
            f = find(binnedPhaseMap_smooth{cond}(cell,:)==0);
            binnedPhaseMap_smooth{cond}(cell,f)=nan;
            rateMap_disc{cond}(cell,:,:) = discretize(rateMap_smooth{cond}(cell,:,:),range);  % discretize both rate/phase to same # of bins...
            phaseMap_disc{cond}(cell,:,:) = discretize(binnedPhaseMap_smooth{cond}(cell,:,:),-pi:.1:pi);% ,-1:.032:1);
-           
+           % discretize shuffled data
+           f = find(rateMap_smooth_shuffle{cond}(cell,:)==0);
+           rateMap_smooth_shuffle{cond}(cell,f)=nan;
+           f = find(binnedPhaseMap_smooth_shuffle{cond}(cell,:)==0);
+           binnedPhaseMap_smooth_shuffle{cond}(cell,f)=nan;
+           for iter = 1:10
+           rateMap_disc_shuffle{cond}(cell,:,iter,:) = discretize(rateMap_smooth_shuffle{cond}(cell,:,iter,:),range);  % discretize both rate/phase to same # of bins...
+           phaseMap_disc_shuffle{cond}(cell,:,iter,:) = discretize(binnedPhaseMap_smooth_shuffle{cond}(cell,:,iter,:),-pi:.1:pi);% ,-1:.032:1);
+           end
         end
 %         phaseMap_disc{cond}(isnan(phaseMap_disc{cond}))=0;    
         
         % run info analysis
         [track_info_rate,pos_info_val_rate] = Info_Analysis(rateMap_disc{cond},1,0);  
         [track_info_phase,pos_info_val_phase] = Info_Analysis(phaseMap_disc{cond},1,0);  
-        
+        for iter =1:10
+            [track_info_rate_shuffle{iter},pos_info_val_rate_shuffle{iter}] = ...
+                Info_Analysis(squeeze(rateMap_disc_shuffle{cond}(:,:,iter,:)),1,0);  
+            [track_info_phase_shuffle{iter},pos_info_val_phase_shuffle{iter}] = ...
+                Info_Analysis(squeeze(phaseMap_disc_shuffle{cond}(:,:,iter,:)),1,0);  
+        end
         
         % compile data
         for cell = 1:length(spikes.times)
@@ -62,6 +85,12 @@ for smoothing = 1:round(nBins/2)
             struct.rateTotalInfo = sum(squeeze(sum(pos_info_val_rate(cell,:,:))));
             struct.ratePeakInfo = max(squeeze(sum(pos_info_val_rate(cell,:,:))));
             
+            for iter = 1:10
+               struct.rateTotalInfo_shuffle(iter) = sum(squeeze(sum(pos_info_val_rate_shuffle{iter}(cell,:,:))));
+               struct.ratePeakInfo_shuffle(iter) = max(squeeze(sum(pos_info_val_rate_shuffle{iter}(cell,:,:))));
+               struct.phaseTotalInfo_shuffle(iter) = sum(squeeze(sum(pos_info_val_phase_shuffle{iter}(cell,:,:))));
+               struct.phasePeakInfo_shuffle(iter) = max(squeeze(sum(pos_info_val_phase_shuffle{iter}(cell,:,:))));
+            end
             struct.smoothing = smoothing;
             struct.condition = cond;
             olypherInfo.results{cell} = [olypherInfo.results{cell}; struct2table(struct)];
@@ -78,6 +107,7 @@ for smoothing = 1:round(nBins/2)
 %                 hold on
 %                 plot(olypherInfo.results{cell}.smoothing(rows),olypherInfo.results{cell}.phasePeakInfo(rows),'g')
 %                 hold off
+%                 title([cell cond])
 %                 pause(.1)
 %             end
             
