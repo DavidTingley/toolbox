5b07dd7c89b444a92a190bc11bf29f6537224767
David Tingley <davidtingley2@gmail.com>
2017-09-07 16:18:45 -0400

    /

--- wrapInfoAnalysis.m
+++ wrapInfoAnalysis.m
@@ -132,7 +132,7 @@ for smoothing = 1:round(nBins/2)
         end
     end
     olypherInfo.dateRun = date;  % this can take a very long time so lets save each loop...
-    save([xml.FileName '.olypherInfo.cellinfo.mat'],'olypherInfo')
+    save([xml.FileName '.olypherInfo_w_disc.cellinfo.mat'],'olypherInfo')
 end
     end
 
